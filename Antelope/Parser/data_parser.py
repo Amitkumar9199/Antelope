6121ff1225324c07770a6eb01f86ab08f2f3ba7c
Jay Kumar Thakur <75112567+jkt10125@users.noreply.github.com>
2024-04-03 16:38:58 +0530

    Update data_parser.py

--- Antelope/Parser/data_parser.py
+++ Antelope/Parser/data_parser.py
@@ -30,6 +30,7 @@
 import re
 import os
 import argparse
+import matplotlib.pyplot as plt
 
 def parse_data(file_path):
     with open(file_path, 'r') as file:
@@ -45,6 +46,47 @@ def write_data(file_path, data):
     with open(file_path, 'w') as file:
         for line in data:
             file.write(line + '\n')
+
+def plot_congestion_control(result):
+    num_timestamps = len(result)
+    timestamps = list(range(num_timestamps))
+
+    plt.figure(figsize=(10, 6))
+    
+    # map<string, vector<int>> congestion_control_map;
+    # input: cubic, westwood, bbr, cubic, bbr, cubic, bbr, bbr, bbr, bbr, westwood ...
+    
+    cc_map = {}
+
+    for i, algorithm in enumerate(result):
+        if algorithm not in cc_map:
+            cc_map[algorithm] = [i]
+        else:
+            cc_map[algorithm].append(i)
+
+    # output: {'cubic': [0, 3, 5], 'westwood': [1, 10], 'bbr': [2, 4, 6, 7, 8, 9]}
+    line = []
+    color = ['r', 'g', 'b', 'y', 'm', 'c', 'k']
+    cc_map_color = {}
+    ctr = 0
+    for algorithm in result:
+        if algorithm not in cc_map_color:
+            cc_map_color[algorithm] = color[ctr]
+            ctr += 1
+
+    for i, algorithm in enumerate(result):
+        line.append(cc_map_color[algorithm])
+
+    print (len(line), len(timestamps))
+
+    # just plot the points
+    # reduce the size of the points
+    plt.scatter(timestamps, result, c=line, s=1)
+    # plt.scatter(timestamps, result, c=line)
+    plt.xlabel('Timestamp')
+    plt.ylabel('Congestion Control Algorithm')
+    plt.title('Congestion Control Algorithm vs Timestamp')
+    plt.show()
 
 def main():
     # file_path = 'sample_result.txt'
@@ -62,7 +104,12 @@ def main():
     result = parse_data(file_path)
     write_data(os.path.join(path, 'parsed_' + file_name), result)
 
+
     print('Data parsed and stored successfully')
+    print(result)
+    plot_congestion_control(result)
+
+
 
 if __name__ == '__main__':
     main()
